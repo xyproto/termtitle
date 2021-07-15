ad7216626c23fddb45c528d9ed41f710e0db0f49
Alexander F. Rødseth <rodseth@gmail.com>
2021-07-15 18:10:49 +0200

    Update the example program

--- cmd/settitle/main.go
+++ cmd/settitle/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/xyproto/termtitle"
 )
 
 func main() {
 	title := "TESTING 1 2 3"
+	if len(os.Args) > 1 {
+		title = strings.Join(os.Args[1:], "")
+	}
 	s, err := termtitle.TitleString(title)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
